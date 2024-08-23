a5348d17a3dfc0984148757ec6ecef4724ba3219
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-08-23 15:55:57 +0900

    style: Pretendard 폰트 파일 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-// const inter = Inter({ subsets: ['latin'] })
-
-const pretendard = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // 필요한 폰트 두께 선택
-  display: 'swap',
-  variable: '--font-pretendard' // CSS 변수로 설정
-})
-
 export const metadata: Metadata = {
   title: '다인리뷰',
   description: '인플루언서와 사업주를 위한 플랫폼, 다인리뷰입니다.'
@@ -22,9 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={pretendard.variable}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   )

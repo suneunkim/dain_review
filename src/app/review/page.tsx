b784ef7adb677443ffb9627e318c784f2de77b8f
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-08-23 15:52:01 +0900

    feat: 드래그 앤 드롭 기능(react-dropzone 라이브러리)

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import ImagePreviewer from '@/components/shared/ImagePreviewer'
 
 const ImageHeader = '/assets/img/header.svg'
@@ -10,14 +10,22 @@ const ImageExCampaign = '/assets/img/ex_campaign.svg'
 // 홈 - 마이페이지(인플루언서) - 리뷰 등록
 
 const page = () => {
-  const handleFileSelect = (file: File | null) => {
-    if (file) {
-      console.log('Selected file:', file)
-    }
+  const [isAgreed, setIsAgreed] = useState(false)
+  const [files, setFiles] = useState<File[]>([])
+
+  const handleFileSelect = (selectedFiles: File[]) => {
+    setFiles(selectedFiles)
   }
 
+  const handleAgreementChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setIsAgreed(event.target.checked)
+  }
+
+  const isSubmitDisabled = !isAgreed || files.length === 0
+
   return (
-    // <Header />
     <main>
       <header className="h-[160px]">
         <h1>헤더 높이 160px</h1>
@@ -49,25 +57,43 @@ const page = () => {
               <h4 className="text-body1 font-[800]">
                 리뷰 URL<strong>*</strong>
               </h4>
-              <p className="text-body2 text-gray-60">
+              <p className="text-body2 mb-[2px] text-gray-60">
                 플랫폼에 작성한 리뷰 URL을 입력해 주세요.
               </p>
-              <input className="mb-[26px] mt-[2px] h-[38px] w-full rounded-[4px] border border-line-normal pl-[12px]" />
-
+              <input
+                className="mb-[26px] h-[38px] w-full rounded-[4px] border border-line-normal pl-[12px]"
+                required
+              />
               <h4 className="text-body1 font-[800]">
                 파일 선택<strong>*</strong>
               </h4>
-              <p className="text-body2 text-gray-60">
+              <p className="text-body2 mb-[2px] text-gray-60">
                 플랫폼에서 본인의 게시물이 나타나는 화면의 캡처 파일을 첨부해
                 주세요.
               </p>
               <ImagePreviewer onFileSelect={handleFileSelect} />
-              <p className="text-body2 text-gray-60">
+              <p className="text-body2 my-[5px] text-gray-60">
                 <strong className="text-gray-60">예시화면</strong>
                 &nbsp;&nbsp;키워드로 검색 시 검색되는 본인의 게시물을 캡처해
                 주세요.
               </p>
-              <button className="mb-[32px] w-full rounded-[4px] bg-red-main px-[20px] py-[12px] text-white">
+              <div className="my-4">
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={isAgreed}
+                    onChange={handleAgreementChange}
+                    className="mr-2"
+                  />
+                  <span className="text-body2 text-gray-60">
+                    개인정보 수집 및 이용에 동의합니다.
+                  </span>
+                </label>
+              </div>
+              <button
+                className="mb-[32px] w-full rounded-[4px] bg-red-main px-[20px] py-[12px] text-white"
+                type="submit"
+                disabled={isSubmitDisabled}>
                 제출
               </button>
             </form>
@@ -78,7 +104,6 @@ const page = () => {
         <h1>푸터 높이 286px</h1>
       </footer>
     </main>
-    // <Footer />
   )
 }
 

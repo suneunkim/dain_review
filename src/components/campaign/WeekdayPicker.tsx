43c2d1aa6eb03e930b41c82e674b56c00cf730be
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-08-30 16:20:01 +0900

    refactor: WeekdayPicker 컴포넌트 코드 개선
    - 매개변수 day를 문자열 리터럴 타입으로 제한
    - onChange 함수의 타입을 명확하게 정의

--- src/components/campaign/WeekdayPicker.tsx
+++ src/components/campaign/WeekdayPicker.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 interface WeekdayPickerProps {
-  day: string
+  day: '일' | '월' | '화' | '수' | '목' | '금' | '토'
   isChecked: boolean
-  onChange: () => void // 체크박스 상태 변경 함수
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void // 체크박스 상태 변경 함수
 }
 
 const WeekdayPicker: React.FC<WeekdayPickerProps> = ({
@@ -11,18 +11,16 @@ const WeekdayPicker: React.FC<WeekdayPickerProps> = ({
   isChecked,
   onChange
 }) => {
+  const textColorClass =
+    day === '일'
+      ? 'text-red-30'
+      : day === '토'
+        ? 'text-blue-50'
+        : 'text-gray-70'
+
   return (
     <label className="flex flex-col items-center">
-      <span
-        className={`${
-          day === '일'
-            ? 'text-red-500'
-            : day === '토'
-              ? 'text-blue-500'
-              : 'text-gray-80'
-        }`}>
-        {day}
-      </span>
+      <span className={textColorClass}>{day}</span>
       <input
         type="checkbox"
         className="form-checkbox"

7a130e785d2c5ee024d74ac6e6d38f0ca5f1f2c5
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-09-06 19:18:57 +0900

    fix: react-dropzone 라이브러리의 accept 속성 타입오류 수정

--- src/components/review/ImagePreviewer.tsx
+++ src/components/review/ImagePreviewer.tsx
@@ -56,7 +56,9 @@ const ImagePreviewer: React.FC<ImagePreviewerProps> = ({
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     onDropRejected,
-    accept: 'image/*',
+    accept: {
+      'image/*': [] // 파일 타입을 지정하는 객체 형식으로 수정
+    },
     maxSize: maxSize * 1024 * 1024 // 최대 파일 크기 설정
   })
 

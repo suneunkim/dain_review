5fb9a29ad60238b6fa26546a0909b3f375166a00
suneunkim <tjsdms89@gmail.com>
2024-09-09 13:17:47 +0900

    style: 검색창 input 길이 수정과 상품카드 최소 너비 지정

--- src/components/home/product/ProductCard.tsx
+++ src/components/home/product/ProductCard.tsx
@@ -10,7 +10,7 @@ const ProductCard = ({ flag }: Props) => {
   return (
     <article className="relative h-[235px] w-full md:h-[354px] md:w-[calc(46.3vw)] lg:h-[365px] lg:w-[22.7vw] desktop:h-[444px] desktop:w-[335px]">
       {/* 이미지 대용 */}
-      <div className="h-[117px] w-full overflow-hidden rounded-t-lg bg-gray-300 md:h-[232px] lg:h-[173px] desktop:h-[252px]" />
+      <div className="h-[117px] w-full min-w-[156px] overflow-hidden rounded-t-lg bg-gray-300 md:h-[232px] lg:h-[173px] desktop:h-[252px]" />
       <div className="mt-[12px]" />
       <Flag flag={flag} />
       <Heart />

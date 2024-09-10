8ebf009cba9495edb1612e41d6c4cba060dd3ce9
suneunkim <tjsdms89@gmail.com>
2024-09-10 20:38:54 +0900

    feat: 홈 배너 슬라이드 구현

---
+++ src/data/bannerSlides.ts
@@ -0,0 +1,31 @@
+const slides = [
+  {
+    description: '인플루언서와 사업주를 위한 플랫폼',
+    titleMobile: '다인리뷰에서\n특별한 경험을 해보세요',
+    titleDesktop: '다인리뷰에서 특별한 경험을 해보세요',
+    buttonText: '지금 가입하고 체험단 참여하기',
+    imgSrc: '/home-banner/image-1.png',
+    imgAlt: '가입하기',
+    imgClassName: 'h-[199.5px] w-[176px] lg:h-[521px] lg:min-w-[489px]'
+  },
+  {
+    description: '신규회원을 위한 다인리뷰 사용백서',
+    titleMobile: '사이트 이용안내',
+    titleDesktop: '사이트 이용안내',
+    buttonText: '사이트 이용안내 바로가기',
+    imgSrc: '/home-banner/image-2.png',
+    imgAlt: '사이트 이용안내',
+    imgClassName: 'h-[199.5px] w-[176px] lg:h-[521px] lg:w-[554px] '
+  },
+  {
+    description: '다인리뷰와 함께라면 포인트 팡팡!',
+    titleMobile: '포인트 이용안내',
+    titleDesktop: '포인트 이용안내',
+    buttonText: '포인트 이용안내 바로가기',
+    imgSrc: '/home-banner/image-3.png',
+    imgAlt: '포인트 이용안내',
+    imgClassName: 'h-[199.5px] w-[176px] lg:h-[521px] lg:min-w-[489px]'
+  }
+]
+
+export default slides

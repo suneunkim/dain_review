92e03a9d386243c15ad64c4fa7dadd846fd47e8b
suneunkim <tjsdms89@gmail.com>
2024-09-22 22:57:59 +0900

    feat: 상품 상세 페이지 에러 메세지 추가

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -3,7 +3,14 @@ import Header from '@/components/shared/Header'
 import { fetchProductDetail } from '@/lib/api'
 
 const page = async ({ params }: { params: { productId: string } }) => {
-  const productDetail = await fetchProductDetail(params.productId)
+  let productDetail
+  let errorMessage = ''
+
+  try {
+    productDetail = await fetchProductDetail(params.productId)
+  } catch (error) {
+    errorMessage = '상품 정보를 불러오는 데 실패했습니다. 다시 시도해 주세요.'
+  }
 
   return (
     <main className="mx-auto w-full min-w-[360px]">
@@ -11,7 +18,12 @@ const page = async ({ params }: { params: { productId: string } }) => {
       <div className="hidden lg:block">
         <Header isLogin />
       </div>
-      <ProductDetailClient data={productDetail} />
+
+      {errorMessage ? (
+        <p className="mt-40 text-center">{errorMessage}</p>
+      ) : (
+        <ProductDetailClient data={productDetail} />
+      )}
     </main>
   )
 }
